7995d8b53b391d412a62aa95d6bba3b3e37d1055
jonasCarfantan <j.carfantan@rhinos.fr>
2020-12-23 01:43:16 +0100

    feat(setup): configurate stuff

--- snowtricks/src/Auth/Action/Login.php
+++ snowtricks/src/Auth/Action/Login.php
@@ -5,9 +5,10 @@ namespace App\Auth\Action;
 use App\_Core\Action\ActionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class Login extends AbstractController implements ActionInterface {
-    
+    #[Route(path: '/login', name: 'login')]
     public function __invoke(): Response {
         // TODO: Implement __invoke() method.
         dd('hi');
